092045e969a0dea5ca68325ae78d16d1ca7385c2
wimil <rldev25@gmail.com>
2021-06-04 17:53:04 -0500

    beta 2

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -4,4 +4,6 @@ else
     yum install certbot python3-certbot-nginx -y
 fi
 
-certbot certonly --webroot --non-interactive --agree-tos --register-unsafely-without-email -w /usr/share/nginx/html -d $server_name
+#certbot certonly --webroot --non-interactive --agree-tos --register-unsafely-without-email -w /usr/share/nginx/html -d $server_name
+
+certbot certonly --nginx -d $server_name  --non-interactive --agree-tos --register-unsafely-without-email
