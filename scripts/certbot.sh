51d42f9037a43dc0b6c478a13b83d7bfbdcb7baf
wimil <rldev25@gmail.com>
2021-06-04 22:43:24 -0500

    alfa2

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,9 +1,8 @@
 if [[ $OS == 'ubuntu' ]]; then
     apt install certbot python3-certbot-nginx -y
+    certbot certonly --nginx -d $server_name  --non-interactive --agree-tos --register-unsafely-without-email
 else
     yum install certbot python3-certbot-nginx -y
 fi
 
 #certbot certonly --webroot --non-interactive --agree-tos --register-unsafely-without-email -w /usr/share/nginx/html -d $server_name
-
-certbot certonly --nginx -d $server_name  --non-interactive --agree-tos --register-unsafely-without-email
--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,8 +1,10 @@
-mv ./templates/letsencrypt-renew /etc/cron.daily/letsencrypt-renew
-chmod +x /etc/cron.daily/letsencrypt-renew
-crontab -l | {
-    cat
-    echo "01 02,14 * * * /etc/cron.daily/letsencrypt-renew"
-} | crontab -
+if [[$OS != 'ubuntu']]; then
+    mv ./templates/letsencrypt-renew /etc/cron.daily/letsencrypt-renew
+    chmod +x /etc/cron.daily/letsencrypt-renew
+    crontab -l | {
+        cat
+        echo "01 02,14 * * * /etc/cron.daily/letsencrypt-renew"
+    } | crontab -
 
-message "success" "Certbot Instalado y configurado"
+    message "success" "Certbot Instalado y configurado"
+fi
