52e7e44bafb5b36fd51aa20feba9a06a5c9ea773
wimil <rldev25@gmail.com>
2021-06-13 08:51:39 -0500

    up

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -3,6 +3,5 @@ if [[ $OS == 'ubuntu' ]]; then
     certbot certonly --nginx -d $server_name  --non-interactive --agree-tos --register-unsafely-without-email
 else
     yum install certbot python3-certbot-nginx -y
+    certbot certonly --webroot --non-interactive --agree-tos --register-unsafely-without-email -w /usr/share/nginx/html -d $server_name
 fi
-
-#certbot certonly --webroot --non-interactive --agree-tos --register-unsafely-without-email -w /usr/share/nginx/html -d $server_name
